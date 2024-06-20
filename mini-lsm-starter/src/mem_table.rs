2eb3fbf44978547c75f7a5d8c0b2bf2fb0208701
Charlie Pecora <charlie.pecora@gmail.com>
2024-06-19 18:18:00 -0700

    part 1 create, insert, get on memtable

--- mini-lsm-starter/src/mem_table.rs
+++ mini-lsm-starter/src/mem_table.rs
@@ -53,7 +53,12 @@ pub(crate) fn map_bound(bound: Bound<&[u8]>) -> Bound<Bytes> {
 impl MemTable {
     /// Create a new mem-table.
     pub fn create(_id: usize) -> Self {
-        unimplemented!()
+        return Self {
+            map: Arc::new(SkipMap::new()),
+            wal: None,
+            id: _id,
+            approximate_size: Arc::new(AtomicUsize::new(0)),
+        };
     }
 
     /// Create a new mem-table with WAL
@@ -87,7 +92,11 @@ impl MemTable {
 
     /// Get a value by key.
     pub fn get(&self, _key: &[u8]) -> Option<Bytes> {
-        unimplemented!()
+        if let Some(v) = self.map.get(_key) {
+            return Some(v.value().clone());
+        } else {
+            return None;
+        }
     }
 
     /// Put a key-value pair into the mem-table.
@@ -96,7 +105,10 @@ impl MemTable {
     /// In week 2, day 6, also flush the data to WAL.
     /// In week 3, day 5, modify the function to use the batch API.
     pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+        let key = bytes::Bytes::copy_from_slice(_key);
+        let value = bytes::Bytes::copy_from_slice(_value);
+        self.map.insert(key, value);
+        return Ok(());
     }
 
     /// Implement this in week 3, day 5; if you want to implement this earlier, use `&[u8]` as the key type.
