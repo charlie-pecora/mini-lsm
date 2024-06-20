bed69a9170d040414e522273642acd46883eb9c0
Charlie Pecora <charlie.pecora@gmail.com>
2024-06-19 18:18:00 -0700

    part 1 create, insert, get on memtable

--- mini-lsm-starter/src/mem_table.rs
+++ mini-lsm-starter/src/mem_table.rs
@@ -38,7 +38,12 @@ pub(crate) fn map_bound(bound: Bound<&[u8]>) -> Bound<Bytes> {
 impl MemTable {
     /// Create a new mem-table.
     pub fn create(_id: usize) -> Self {
-        unimplemented!()
+        return Self {
+            map: Arc::new(SkipMap::new()),
+            wal: None,
+            id: _id,
+            approximate_size: Arc::new(AtomicUsize::new(0)),
+        };
     }
 
     /// Create a new mem-table with WAL
@@ -69,7 +74,11 @@ impl MemTable {
 
     /// Get a value by key.
     pub fn get(&self, _key: &[u8]) -> Option<Bytes> {
-        unimplemented!()
+        if let Some(v) = self.map.get(_key) {
+            return Some(v.value().clone());
+        } else {
+            return None;
+        }
     }
 
     /// Put a key-value pair into the mem-table.
@@ -77,7 +86,10 @@ impl MemTable {
     /// In week 1, day 1, simply put the key-value pair into the skipmap.
     /// In week 2, day 6, also flush the data to WAL.
     pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+        let key = bytes::Bytes::copy_from_slice(_key);
+        let value = bytes::Bytes::copy_from_slice(_value);
+        self.map.insert(key, value);
+        return Ok(());
     }
 
     pub fn sync_wal(&self) -> Result<()> {
